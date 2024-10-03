2a5a505c31bdac63d3813ad56f6e3ee2afdbdeb8
tinph010803 <tinphan924@gmail.com>
2024-10-03 16:27:03 +0700

    Add OnboardingScreen

--- frontend/src/screens/OnboardingScreen.tsx
+++ frontend/src/screens/OnboardingScreen.tsx
@@ -1,12 +1,44 @@
-import {View, Text} from 'react-native';
+import { View, Text, Image } from 'react-native';
 import React from 'react';
+import Onboarding from 'react-native-onboarding-swiper';
+import { images } from '../constants';
+import { SplashData } from '../constants/data';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+
 
 type Props = {};
+export type RootStackParamList = {
+  Login: { id: number } | undefined;
+
+};
 
 const OnboardingScreen = (props: Props) => {
   return (
-    <View>
-      <Text>OnboardingScreen</Text>
+    <View className="flex-1">
+      <Onboarding
+        pages={[
+          {
+            backgroundColor: '#fff',
+            image: <Image source={SplashData[0].image} />,
+            title: SplashData[0].title,
+            subtitle: SplashData[0].description,
+          },
+          {
+            backgroundColor: '#fff',
+            image: <Image source={SplashData[1].image} />,
+            title: SplashData[1].title,
+            subtitle: SplashData[1].description,
+          },
+          {
+            backgroundColor: '#fff',
+            image: <Image source={SplashData[2].image} />,
+            title: SplashData[2].title,
+            subtitle: SplashData[2].description,
+          },
+        ]}
+      />
     </View>
   );
 };
