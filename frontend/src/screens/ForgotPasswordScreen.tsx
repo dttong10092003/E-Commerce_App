71b8b0f270d32165d69700fc5cb2df9c347ac188
DESKTOP-BPE6J3K\DELL <tong10092003@gmail.com>
2024-10-04 17:07:17 +0700

    Update formfield and custombutton for Screen ForgotPassword

--- frontend/src/screens/ForgotPasswordScreen.tsx
+++ frontend/src/screens/ForgotPasswordScreen.tsx
@@ -3,7 +3,7 @@ import {View, Text, Image, TouchableOpacity, TextInput, StyleSheet} from 'react-
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
-import {CustomButton} from '../components';
+import {CustomButton, FormField} from '../components';
 import icons from '../constants/icons'; 
 
 type Props = {}
@@ -26,39 +26,39 @@ const ForgotPasswordScreen = (props: Props) => {
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-
-      <TouchableOpacity style={styles.backButton} onPress={() => navigation.navigate('Login')}>
+    <SafeAreaView className="px-5 flex-1 bg-white">
+      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
         <Image source={icons.back} style={styles.backIcon} /> 
       </TouchableOpacity>
-      <Text style={styles.title}>Forgot Password?</Text>
-      <View style={styles.inputWrapper}>
-        <View style={styles.inputContainer}>
-          <Image
-            source={icons.mail}
-            style={styles.icon}
-            resizeMode="contain"
-          />
-          <TextInput
-            value={form.email}
-            onChangeText={(text) => setForm({...form, email: text})}
-            placeholder="Enter your email address"
-            style={styles.input}
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
-        </View>
-        <Text style={styles.noteText}>
-          <Text style={styles.asterisk}>*</Text> We will send you a message to set or reset your new password
-        </Text>
+
+      <Text className='text-4xl font-bold text-start'>
+        Forgot{'\n'}Password?
+      </Text>
+
+      <View>
+        <FormField
+          title="Email"
+          value={form.email}
+          setError={setEmailError}
+          error={emailError}
+          handleChangeText={(e: any) => {
+            setEmailError('');
+            setForm({...form, email: e});
+          }}
+          placeholder="Enter your email address"
+          otherStyles="my-5"
+        />
       </View>
+
+      <Text style={styles.noteText}>
+          <Text style={styles.asterisk}>*</Text> We will send you a message to set or reset your new password
+      </Text>
 
       <CustomButton
         title="Submit"
         handlePress={handleForgotPasswordSubmit}
         isLoading={isSubmitting}
-        containerStyle="py-5 bg-red-500"
-        textStyle="text-white text-lg"
+        containerStyle="py-5"
       />
     </SafeAreaView>
   );
@@ -66,58 +66,25 @@ const ForgotPasswordScreen = (props: Props) => {
 
 export default ForgotPasswordScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 20,
-    paddingTop: 20, 
-    backgroundColor: '#fff', 
-  },
+const styles = StyleSheet.create({ 
   backButton: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 20,
+    marginBottom: 10,
   },
   backIcon: {
     width: 24,
     height: 24,
     marginRight: 5,
   },
-  title: {
-    fontSize: 32, 
-    fontWeight: 'bold', 
-    textAlign: 'left',
-  },
-  inputWrapper: {
-    marginTop: 40, 
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 8,
-    paddingVertical: 12,
-    paddingHorizontal: 15,
-    marginBottom: 10,
-  },
-  icon: {
-    width: 20,
-    height: 20,
-    marginRight: 10,
-  },
-  input: {
-    flex: 1,
-    fontSize: 16,
-  },
   noteText: {
-    color: '#575757',
+    color: '#676767',
     fontSize: 14, 
     marginTop: 10,
     marginBottom: 40, 
   },
   asterisk: {
-    color: 'red',
+    color: '#FF4B28',
     fontSize: 14,
   },
  
